173084599b9477d0b7efed055844f7166ddf0a81
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2021-11-08 12:52:32 -0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Button, Container, Heading} from "@chakra-ui/react"
 import { useMoralis } from "react-moralis";
 //import MyTodoList from './contract_build/MyTodoList.json';
-import {Moralis} from 'moralis'
+
 import './App.css';
 import{Login} from './Login.js'
 
